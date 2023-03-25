d80c8b549f5c658cb725ca1c1397e7f21d29b01b
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-03-25 15:49:36 +0100

    new branch name

--- models/Projects.model.js
+++ models/Projects.model.js
@@ -4,8 +4,12 @@ const projectSchema = new Schema ({
     title: String,
     description: String,
     technologies: String,
-    url: String,
+    urlGit: String,
     image: String,
+    linkedIn: {
+        type: String,
+        default:"https://www.linkedin.com/in/eogimenez/"
+    }
 })
 
 module.exports = model("Project", projectSchema);
