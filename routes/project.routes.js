28bd9ca778a249ca4ec60f831837133139ae5b85
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-04-10 11:57:08 +0200

    status code

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -31,12 +31,12 @@ router.post('/upload', fileUploader.single('image'), (req, res, next) => {
 router.post('/new', (req, res, next) => {
 	const { title, description, secDescription, technologies, urlGit, image, ownCode } = req.body;
 	if (!bcrypt.compareSync(ownCode, process.env.CRYPTCODE)) {
-		res.json({ messageError: 'Your Owner Code is not correct !' });
+		res.status(403).json({ messageError: 'Your Owner Code is not correct !' });
 		return;
 	}
 	Project.create({ title, description, secDescription, technologies, urlGit, image })
 		.then((response) => {
-			res.json({ response: 'Created !' });
+			res.status(201).json({ response: 'Created !' });
 		})
 		.catch((err) => console.log('paso esto', err));
 });
@@ -45,7 +45,7 @@ router.put('/:projId/edit', (req, res, next) => {
 	const { projId } = req.params;
 	const { title, description, secDescription, technologies, urlGit, image, ownCode } = req.body;
 	if (!bcrypt.compareSync(ownCode, process.env.CRYPTCODE)) {
-		res.json({ messageError: 'Your Owner Code is not correct !' });
+		res.status(403).json({ messageError: 'Your Owner Code is not correct !' });
 		return;
 	}
 	Project.findByIdAndUpdate(
@@ -63,12 +63,12 @@ router.delete('/:projId/delete', (req, res, next) => {
 	const { projId } = req.params;
 	const { ownCode } = req.body;
 	if (!bcrypt.compareSync(ownCode, process.env.CRYPTCODE)) {
-		res.json({ messageError: 'Your Owner Code is not correct' });
+		res.status(403).json({ messageError: 'Your Owner Code is not correct' });
 		return;
 	}
 	Project.findByIdAndDelete(projId)
 		.then((result) => {
-			res.json({ message: `Project was deleted` });
+			res.status(202).json({ message: `Project was deleted` });
 		})
 		.catch((err) => next(err));
 });
