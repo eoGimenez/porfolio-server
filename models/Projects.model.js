199f5f7baf02a7f74197d955ac9cbe4341eaac0d
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-04-10 12:26:59 +0200

    model required

--- models/Projects.model.js
+++ models/Projects.model.js
@@ -1,12 +1,12 @@
 const { Schema, model } = require('mongoose');
 
 const projectSchema = new Schema({
-	title: String,
-	description: String,
-	secDescription: String,
+	title: { type: String, required: true },
+	description: { type: String, required: true },
+	secDescription: { type: String, required: true },
 	technologies: [String],
-	urlGit: String,
-	image: String,
+	urlGit: { type: String, required: true },
+	image: { type: String, required: true },
 	linkedIn: {
 		type: String,
 		default: 'https://www.linkedin.com/in/eogimenez/',
