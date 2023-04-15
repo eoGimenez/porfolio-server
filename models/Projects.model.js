713ace6791f930cf98fa89aee790dc939dee3684
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-04-15 21:59:58 +0200

    Resuelto

--- models/Projects.model.js
+++ models/Projects.model.js
@@ -4,7 +4,7 @@ const projectSchema = new Schema({
 	title: { type: String, required: true },
 	description: { type: String, required: true },
 	secDescription: { type: String, required: true },
-	technologies: [String],
+	technologies: [{ type: String, required: true }],
 	urlGit: { type: String, required: true },
 	image: { type: String, required: true },
 	linkedIn: {
